5d70711db89f8307a76223332b53d6df09d7e424
Alejandro Rey <74797718+reyalejandro@users.noreply.github.com>
2022-04-28 12:31:47 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,2 @@
+docker run --rm -v "$WORKSPACE":/usr/src -w /usr/src markhobson/maven-chrome:jdk-8 mvn clean test -Dcucumber.options="--tag '$TAG'"
+curl -D- -X POST "https://laboratoriobfcl.atlassian.net/rest/api/3/issue/$TESTPLAN/attachments" -H 'X-Atlassian-Token: no-check' -F "file="@$WORKSPACE/results/ExtentPdf.pdf"" -H 'application/octet-stream' -H 'Authorization: Basic a2V2aW4ucGVuYUB0c29mdGxhdGFtLmNvbTp5eVZDWWNGU2dPMkRVbk5KN3R4RTA3MjU= '
